bd9f74550918f15770df1247f76e750569ca6a01
saul rosales <saul.rosalesb@gmail.com>
2023-12-13 20:04:49 -0500

    Sample application with google maps search

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+// Code goes here!
+const form = document.querySelector('form')!
+const addressInput = document.getElementById('address')! as HTMLInputElement
+const GOOGLE_API_KEY = 'AIzaSyBoei01li4Kt-T5USVYySkTNtWYLPIMNJ8'
+//declare var google: any
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[]
+  status: 'OK' | 'ZERO_RESULTS'
+}
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault()
+  const enteredAddress = addressInput.value
+  console.log(enteredAddress)
+  // send this to Google's API!
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!')
+      }
+      const coordinates = response.data.results[0].geometry.location
+      const map = new google.maps.Map(document.getElementById('map')!, {
+        center: coordinates,
+        zoom: 16
+      })
+      new google.maps.Marker({ position: coordinates, map })
+
+      console.log(coordinates)
+    })
+    .catch(err => {
+      alert(err.message)
+      console.log(err)
+    })
+}
+
+form.addEventListener('submit', searchAddressHandler)
